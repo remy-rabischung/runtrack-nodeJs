7a8990e887fe9d468b64fa2d303b6522c2ab0adc
Rémy R <remy.rabischung@laplateforme.io>
2024-05-17 10:48:54 +0200

    Job5 terminé

--- runtrack_express/projet_web/index.js
+++ runtrack_express/projet_web/index.js
@@ -1,14 +1,34 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
 const app = express();
 
-app.listen(80, () => {
-    console.log('Server is running on port 80');
-  });
+// Serveur statique pour les fichiers publics
+app.use(express.static(path.join(__dirname, 'public')));
 
-  app.get('/', (req, res) => {
-    res.send('Bienvenue sur la page d’accueil de notre site !');
+// Routes
+app.get('/', (req, res) => {
+  fs.readFile(path.join(__dirname, 'views', 'index.html'), 'utf8', (err, html) => {
+    if (err) {
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.send(html);
+    }
   });
-  
-  app.get('/about', (req, res) => {
-    res.send('Ce projet est une application web.');
+});
+
+app.get('/about', (req, res) => {
+  fs.readFile(path.join(__dirname, 'views', 'about.html'), 'utf8', (err, html) => {
+    if (err) {
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.send(html);
+    }
   });
+});
+
+// Démarrer le serveur
+app.listen(80, () => {
+  console.log('Server is running on port 80');
+});
