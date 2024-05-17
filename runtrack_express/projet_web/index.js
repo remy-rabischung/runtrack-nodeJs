5a8d0c1f0918ff3b6a94ba57f67256d1fb337bc0
Rémy R <remy.rabischung@laplateforme.io>
2024-05-17 10:37:20 +0200

    Job4 terminé

--- runtrack_express/projet_web/index.js
+++ runtrack_express/projet_web/index.js
@@ -4,3 +4,11 @@ const app = express();
 app.listen(80, () => {
     console.log('Server is running on port 80');
   });
+
+  app.get('/', (req, res) => {
+    res.send('Bienvenue sur la page d’accueil de notre site !');
+  });
+  
+  app.get('/about', (req, res) => {
+    res.send('Ce projet est une application web.');
+  });
